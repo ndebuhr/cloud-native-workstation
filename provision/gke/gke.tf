b00da61d0e2847b57271096da6a3eef7c3b485d3
ndebuhr <ndebuhr@protonmail.com>
2022-01-01 17:17:19 +0000

    ⏪️ Revert "📏 Reduce the max pods per node in GKE provisioning so that smaller CIDR ranges are provisioned (workstations do not have high pod counts)"
    This reverts commit eb6fd8a7b0e7d756cfd3ecbd8c3e790899cb8e26.

--- provision/gke/gke.tf
+++ provision/gke/gke.tf
@@ -3,13 +3,12 @@ provider "google" {}
 data "google_project" "project" {}
 
 resource "google_container_cluster" "primary" {
-  name                      = var.gke_cluster_name
-  project                   = data.google_project.project.number
-  location                  = var.gcp_zone
-  remove_default_node_pool  = true
-  initial_node_count        = 1
-  enable_shielded_nodes     = true
-  default_max_pods_per_node = 64
+  name                     = var.gke_cluster_name
+  project                  = data.google_project.project.number
+  location                 = var.gcp_zone
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  enable_shielded_nodes    = true
   network_policy {
     enabled = true
   }
